3acb0ce03c0010d1043bb0de4ad9ebcdaf699f1c
Brian Bud <hello@brianbud.com>
2023-11-22 19:57:35 -0500

    menu data on json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from "lodash";
 import "./style.css";
 import BackgroundImg from "./images/forest.jpg";
+import Data from "./data.json";
+
+console.log(Data.menu[1]);
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpacassdfasdfk!!!!!!"], " ");
+  element.innerHTML = _.join(["Hello", "world!!!!!!"], " ");
   element.classList.add("hello");
 
   const bgImg = new Image();
