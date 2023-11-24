ea792ed1e9388476754616127ae4de43481688f1
Brian Bud <hello@brianbud.com>
2023-11-23 21:45:55 -0500

    nav links

--- src/index.js
+++ src/index.js
@@ -12,6 +12,24 @@ function createheader() {
   title.textContent = "The Bear";
   header.appendChild(title);
 
+  let nav = document.createElement("nav");
+  let list = document.createElement("ul");
+
+  let home = document.createElement("li");
+  let menu = document.createElement("li");
+  let about = document.createElement("li");
+
+  home.innerHTML = `<a href="#home">Home</a>`;
+  menu.innerHTML = `<a href="#menu">menu</a>`;
+  about.innerHTML = `<a href="#about">about</a>`;
+
+  list.appendChild(home);
+  list.appendChild(menu);
+  list.appendChild(about);
+
+  nav.appendChild(list);
+  header.appendChild(nav);
+
   return header;
 }
 
